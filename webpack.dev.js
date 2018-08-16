2e7f42224fd4d96b19747944a6f1d2bd8523e7c0
dondakeshimo <went.went.takkun135@gmail.com>
2018-08-17 00:29:26 +0900

    [fix]favicon追加、バグを修正

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
                 loaders: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
-    }
+    },
     devServer: {
             contentBase: './dist',
         }
